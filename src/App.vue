7f2639ca3214fd4a902f034acc13560d927ef834
yeviki <yeviki.maisyahputra@gmail.com>
2025-11-19 10:47:06 +0700

    Frontend Simpau Bismillah

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/src/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline text-blue-500">
+      Hello world!
+    </h1>
+  </body>
+  </html>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
